cb01191de068472cfbf8fe0299ac7f51e3f1bb66
fatemaAlbasri <falbasri4@gmail.com>
2025-07-21 22:30:15 +0300

    edit image profile

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const authCtrl = require('../controllers/auth')
+const upload = require('../middleware/upload')
 
 // Routes - Call API's
 
 router.get('/sign-up', authCtrl.auth_signup_get)
-router.post('/sign-up', authCtrl.auth_signup_post)
+router.post('/sign-up', upload.single('image'), authCtrl.auth_signup_post)
 
 router.get('/sign-in', authCtrl.auth_signin_get)
 router.post('/sign-in', authCtrl.auth_signin_post)
@@ -12,6 +13,6 @@ router.post('/sign-in', authCtrl.auth_signin_post)
 router.get('/sign-out', authCtrl.auth_signout_get)
 
 router.get('/edit', authCtrl.auth_edit_get)
-router.put('/edit', authCtrl.auth_edit_put)
+router.put('/edit', upload.single('image'), authCtrl.auth_edit_put)
 
 module.exports = router
