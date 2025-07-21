4bf0ca3a085817fb25de26ada65d20371de78dbe
FerasGIT <ferasny@gmail.com>
2025-07-21 23:46:35 +0300

    done with movie controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,6 @@
 const Movie = require('../models/movie')
 const Booking = require('../models/booking')
 
-
 exports.movie_create_get = async (req, res) => {
   if (req.session.user.role === 'user') {
     res.send('Error page not found!!!')
@@ -16,7 +15,7 @@ exports.movie_create_post = async (req, res) => {
     // Check if the movie already exists
     if (!(await Movie.findOne({ name: req.body.name }))) {
       await Movie.create(req.body)
-      const movieId = Movie.findOne({name:req.body.name})
+      const movieId = await Movie.findOne({ name: req.body.name })
       await Booking.create({
         theater: req.body.theater,
         time: req.body.time,
@@ -32,16 +31,21 @@ exports.movie_create_post = async (req, res) => {
   }
 }
 exports.movie_index_get = async (req, res) => {
-  const listOfMovies = await Movie.find()
-  res.render('movie/index.ejs', { movies: listOfMovies })
+  const listOfMovies = await Booking.find().populate('movie')
+  const bookings = await Booking.find()
+  res.render('movie/index.ejs', { movies: listOfMovies, bookings })
 }
 
-
 exports.movie_delete_delete = async (req, res) => {
-  const currentMovie = await Movie.findById(req.params.movieId);
-  if(currentMovie.admin.equals(req.session.user_id)){
+  const currentMovie = await Movie.findById(req.params.movieId)
+  if (currentMovie.admin.equals(req.session.user_id)) {
     //need to change the following  ''await currentListing.updateOne(req.body);''
-  }else{
+  } else {
     //res.send("");
   }
+}
+exports.movie_booking_get = async (req, res) => {
+  const booking = await Booking.find(req.params.booingId)
+  const movie = await Booking.find(req.params.booingId).populate('movie')
+  res.render('movie/booking.ejs', { booking, movie })
 }
--- routes/moiveRoute.js
+++ routes/moiveRoute.js
@@ -5,5 +5,5 @@ const router = require('express').Router()
 router.get('/new', movieCtrl.movie_create_get)
 router.post('/new', movieCtrl.movie_create_post)
 router.get('/', movieCtrl.movie_index_get)
-
+router.get('/:bookingId', movieCtrl.movie_booking_get)
 module.exports = router
